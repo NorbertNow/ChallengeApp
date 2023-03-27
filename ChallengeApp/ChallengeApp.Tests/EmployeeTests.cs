cc50c9afa2e54e214a5f36cc5c76cd28b0bc7e24
Norbert Nowak <nor.nowak@gmail.com>
2023-03-27 18:57:15 +0100

    Day 8 Homework (Type Tests)

--- ChallengeApp/ChallengeApp.Tests/EmployeeTests.cs
+++ ChallengeApp/ChallengeApp.Tests/EmployeeTests.cs
@@ -1,6 +1,6 @@
 namespace ChallengeApp.Tests
 {
-    public class Tests
+    public class EmployeeTests
     {
         // ================ TEST 1 =======================================================
         [Test]
---
+++ ChallengeApp/ChallengeApp.Tests/TypeTests.cs
@@ -0,0 +1,76 @@
+﻿namespace ChallengeApp.Tests
+{
+    internal class TypeTests
+    {
+        // ------------------- VALUE TYPE (INTEGER) TEST --------------------------------
+        [Test]
+        public void IntegerTypeTest()
+        {
+            // ARRANGE
+        
+            int number1 = 1;
+            int number2 = 1;
+            
+            // ACT
+
+            // ASSERT
+
+            Assert.AreEqual(number1, number2);
+        }
+
+        // ------------------- VALUE TYPE (FLOAT) TEST --------------------------------
+        [Test]
+        public void FloatTypeTest()
+        {
+            // ARRANGE
+
+            float number1 = 2.5456f;
+            float number2 = 2.5456f;
+
+            // ACT
+
+            // ASSERT
+
+            Assert.AreEqual(number1, number2);
+        }
+
+        // ------------------- IMMUTABLE TYPE (STRING) TEST --------------------------------
+        [Test]
+        public void StringTypeTest()
+        {
+            // ARRANGE
+
+            string name1 = "Jan";
+            string name2 = "Jan";
+
+            // ACT
+
+            // ASSERT
+
+            Assert.AreEqual(name1, name2);
+        }
+
+        // --------------------- REFERENCE TYPE TEST -------------------------------
+        [Test]
+        public void ReferenceTypeTest() 
+
+        // ARRANGE
+        {
+            var employee1 = GetEmployee("Jan", "Kowalski", 30);
+            var employee2 = GetEmployee("Jan", "Kowalski", 30);
+        // ACT
+
+        // ASSERT
+
+        Assert.AreNotEqual(employee1, employee2);
+        
+        }
+        
+        // --------------------- PRIVATE METHOD -------------------------------------------
+        private Employee GetEmployee (string name, string surname, int age) 
+        {
+            return new Employee (name, surname, age);
+        }
+    
+    }
+}
